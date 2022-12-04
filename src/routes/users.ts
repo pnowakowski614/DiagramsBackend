5f6f951ebfc71271806a6caa6bbd52da82160587
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-12-04 14:25:11 +0100

    WIP: testing

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -23,12 +23,10 @@ router.post('/login', async (request, response) => {
         console.log(user);
 
         if(!user) {
-            console.log("no user");
             return response.status(500).json({status: 'error', user: false})
         }
 
         const isPasswordValid = await bcrypt.compare(request.body.password, user.password);
-        console.log(isPasswordValid);
 
         if (isPasswordValid) {
             const token = jwt.sign({
